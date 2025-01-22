d4c397441320c489edacb517387dbcfe29d1efce
Salvador Di Renzo <direnzosalvatore@gmail.com>
2025-01-22 22:48:41 +0100

    add initial views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :info, :error, :success
+
+  before_action :authenticate_user!
+
+  # Ensure Turbo works correctly with Devise
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    root_path
+  end
 end
