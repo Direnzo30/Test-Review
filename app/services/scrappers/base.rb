fa992a60b44d03e03a1d6bc4285c9d977c357312
Salvador Di Renzo <direnzosalvatore@gmail.com>
2025-01-26 03:25:32 +0100

    add navbar and fix some turbo issues

--- app/services/scrappers/base.rb
+++ app/services/scrappers/base.rb
@@ -8,7 +8,9 @@ module Scrappers
 
     def initialize(url)
       options = Selenium::WebDriver::Chrome::Options.new
-      options.add_argument('--headless') # Run browser in headless mode
+      options.add_argument('--headless')
+      options.add_argument('--disable-gpu')
+      options.add_argument('--no-sandbox')
       @driver = Selenium::WebDriver.for :chrome, options: options
       @scrapping_wait = Selenium::WebDriver::Wait.new(timeout: SELENIUM_TIME_OUT)
       @url = url
